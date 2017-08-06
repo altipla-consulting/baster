bb939394eff611f5dcfb146b873863697b7f3640
Ernesto Alejo <ernestokarim@gmail.com>
2017-08-06 02:02:31 +0200

    Start the skeleton of the kubernetes watcher that will find new services to proxy.

--- cmd/baster/main.go
+++ cmd/baster/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"net/http"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/juju/errors"
 )
 
 func main() {
 	if err := run(); err != nil {
-	  log.Fatal(errors.ErrorTrace(err))
+		log.Fatal(errors.ErrorTrace(err))
 	}
 }
 
 func run() error {
+	watcher := NewKubernetesWatcher()
+	watcher.Run()
+
+	server := &http.Server{
+		Addr:         ":9000",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		return errors.Trace(err)
+	}
 	return nil
 }
