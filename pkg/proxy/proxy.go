2a90e2926ba8f29cba6bf962f436b6707ab78bfa
Ernesto Alejo <ernestokarim@gmail.com>
2018-02-18 13:54:32 +0100

    Emite por consola los orígenes del CORS en lugar de la estructura entera.

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -46,7 +46,7 @@ func Handler(domain config.Domain) http.HandlerFunc {
 		"service":              domain.Service,
 		"reject-static-assets": domain.RejectStaticAssets,
 		"virtual-hostname":     domain.VirtualHostname,
-		"cors":                 domain.CORS,
+		"cors-origins":         domain.CORS.Origins,
 	}).Info("Domain configured")
 
 	if len(domain.Paths) == 0 {
